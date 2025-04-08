68e58708a38ae0580421456af0168388899ad1c1
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-04-08 11:07:08 -0300

    Update inserir_agendamento.php
    Fiz correções no funcionamento do PhP para ele capturar também o nome do paciente.

--- inserir_agendamento.php
+++ inserir_agendamento.php
@@ -4,25 +4,27 @@ $usuario = "root";
 $senha = "";
 $banco = "agendamentos";
 
-// Conectar ao banco
+
 $conn = new mysqli($host, $usuario, $senha, $banco);
 
-// Verifica conexão
+
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Pega os dados do formulário
+
 $usuario_id = $_POST['usuario_id'];
 $data_hora = $_POST['data_hora'];
-$descricao = $_POST['descricao'];
+$nome = $_POST['nome'];
+$descricao = $_POST['consulta'];
 
-// Prepara o SQL
-$sql = "INSERT INTO agendamentos (usuario_id, data_hora, descricao) VALUES (?, ?, ?)";
+
+$sql = "INSERT INTO agendamentos (usuario_id, data_hora, nome, descricao)
+        VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("iss", $usuario_id, $data_hora, $descricao);
+$stmt->bind_param("isss", $usuario_id, $data_hora, $nome, $descricao);
 
-// Executa e verifica sucesso
+
 if ($stmt->execute()) {
     echo "Agendamento feito com sucesso!";
 } else {
