8007b39f841271850cb0ba1edac24572736f44e7
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-06-09 16:26:28 -0300

    Update Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -2,14 +2,14 @@
 $host = 'localhost';
 $port = '5432';
 $dbname = 'sistema_clinico';
-$user = 'postgres'; // ou seu usuário
-$password = 'sua_senha'; // substitua pela sua senha real
+$user = 'postgres'; 
+
+$password = 'sua_senha'; 
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conectado com sucesso!";
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
 }
 ?>
