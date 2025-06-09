a6e7eff1b793300d2fae457cfa5e239b1d72ccb0
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-06-09 16:27:40 -0300

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,17 +1,9 @@
 <?php
 
 session_start();
-if (!isset($_SESSION['usuario_id'])) {
-    header('Location: login.html');
-    exit;
-}
+session_unset();
+session_destroy();
 
-
-session_start();
-session_unset();    // Remove todas as variáveis da sessão
-session_destroy();  // Destrói a sessão
-
-// Redireciona para a página de login após logout
 header('Location: login.html');
 exit;
 ?>
