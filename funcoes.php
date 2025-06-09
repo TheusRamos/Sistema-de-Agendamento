945470668e2809d846c80e2f1a758bd4bbd3f7d1
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-06-09 16:09:25 -0300

    Delete funcoes.php

--- funcoes.php
+++
@@ -1,8 +0,0 @@
-<?php
-function validarNome($nome) {
-    return preg_match("/^[a-zA-ZÀ-ú\s]+$/", $nome);
-}
-function formatarDataHora($dataHora) {
-    return date('d/m/Y H:i', strtotime($dataHora));
-}
-?>
